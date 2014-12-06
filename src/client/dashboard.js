6158e2ab7f5609d9b2d425ddcd38dbabc940fe84
John Babak <babak.john@gmail.com>
2014-12-06 05:18:14 +0300

    Abstracted away layout from data
    Separated chart data temporary storage and actual data storage.
    Fixed layout size recalc after render.
    Fixed chart data stubbing when there is not enough data for display.

--- src/client/dashboard.js
+++ src/client/dashboard.js
@@ -5,6 +5,7 @@ var inherits = require('inherits');
 var Rickshaw = require('rickshaw');
 var moment = require('moment');
 var console = global.console;
+var setTimeout = global.setTimeout;
 
 require('./dashboard.less');
 
@@ -16,78 +17,176 @@ function Dashboard(options) {
 	
 	_this.selector = 'body';
 	
-	_this.layout = {
-		row: [
-			{
-				cell: {
-					header: {
-						text: 'CPU Load'
-					},
-					chart: {
-						seriesId: 'cpu',
-						color: 'blue',
-						min: 0.0,
-						max: 1.0,
-						seconds: 15
-					}
-				}
-			},
-			//{
-				//col: [
-				//	{ cell: {} },
-				//	{ cell: {} },
-				//	{ cell: {} }
-				//]
-			//},
-			{
-				cell: {
-					header: {
-						text: 'Memory Footprint'
-					},
-					chart: {
-						seriesId: 'memory',
-						min: 0.0,
-						max: 1.0,
-						seconds: 15
-					}
-				}
-			}
-		]
-	};
+	_this._layout = {};
+	_this._layoutLoading = false;
+	
+	_this._series = {};
 	
 	_.extend(_this, options);
 	
 	_this.$el = $(_this.selector).addClass('wsdash-root');
 	
-	_this._renderLayout(_this.layout, _this.$el);
-	
-	_this._resizeLayout(_this.layout);
+	_this._renderLayout();
 	
 	$(global).on('resize orientationchange', _.throttle(function () {
-		_this._resizeLayout(_this.layout);
+		_this._resizeLayout();
 	}, 10));
+	
+	_this.loadLayout();
 }
 inherits(Dashboard, EventEmitter);
 _.extend(Dashboard.prototype, {
-	updateCharts: function (timestamp, seriesData) {
+	loadLayout: function () {
 		var _this = this;
 		
-		_this._traverseLayout(_this.layout, {},
+		if (_this._layoutLoading) { return; }
+		
+		_this._layoutLoading = true;
+		_this._layoutPrevious = _this._layout;
+		_this._layout = {
+			col: [
+				{
+					cell: {
+						header: {
+							text: 'Loading layout...'
+						}
+					}
+				}
+			]
+		};
+		_this._renderLayout();
+		
+		var deferred = $.Deferred();
+		
+		deferred
+			.then(function (layout) {
+				_this._layout = layout;
+				_this._layoutLoading = false;
+				
+				_this._renderLayout();
+			}, function (error) {
+				_this._layout = _this._layoutPrevious;
+				_this._layoutLoading = false;
+				
+				_this._renderLayout();
+				
+				if (global.confirm('An error occurred while loading the layout. Try again?')) {
+					_this.loadLayout();
+				}
+			});
+		
+		setTimeout(function () {
+			
+			// TODO: Request the layout from the server-side.
+			var layout = {
+				col: [
+					{
+						row: [
+							{
+								cell: {
+									header: {
+										text: 'CPU Load'
+									},
+									chart: {
+										seriesId: 'cpu',
+										color: 'blue',
+										min: 0.0,
+										max: 1.0,
+										ttl: 10
+									}
+								}
+							},
+							{
+								cell: {
+									header: {
+										text: 'Memory Footprint'
+									},
+									chart: {
+										seriesId: 'memory',
+										min: 0.0,
+										max: 1.0,
+										ttl: 10
+									}
+								}
+							}
+						]
+					},
+					{
+						row: [
+							{
+								cell: {
+									header: {
+										text: 'CPU Load copy'
+									},
+									chart: {
+										seriesId: 'cpu',
+										color: 'red',
+										min: 0.0,
+										max: 1.0,
+										ttl: 20
+									}
+								}
+							}
+						]
+					}
+				]
+			};
+			
+			deferred.resolve(layout);
+			
+		}, 2000);
+	},
+	
+	updateCharts: function (timestamp, updates) {
+		var _this = this,
+			layout = _this._layout,
+			SERIES_DATA_DEFAULT_TTL = 30;
+		
+		function mapper(value) {
+			return { x: Math.floor(timestamp / 1000), y: value };
+		}
+		
+		for (var seriesId in updates) { if (updates.hasOwnProperty(seriesId)) {
+			var update = updates[seriesId];
+			if (update && update.data) {
+				var series = _this._series[seriesId] = _this._series[seriesId] || {};
+				series.data = series.data || [];
+				series.ttl = update.ttl || series.ttl || SERIES_DATA_DEFAULT_TTL;
+				
+				var updateData = update.data.map(mapper);
+				
+				var seriesData = series.data;
+				seriesData.push.apply(seriesData, updateData);
+				
+				while ((seriesData[seriesData.length-1].x - seriesData[0].x) > series.ttl) {
+					seriesData.shift();
+				}
+			}
+		} }
+		
+		_this._traverseLayout(layout, {},
 			null,
 			function (ctx, layout, item, cell) {
-				if (cell && cell.chart && cell.chart.seriesId && cell.chart.graph) {
-					var series = seriesData[cell.chart.seriesId];
-					if (series && series.data) {
-						var updatesData = series.data.map(function (value) {
-							return { x: Math.floor(timestamp / 1000), y: value };
-						});
+				if (cell && cell.chart && cell.chart.graph) {
+					var series = _this._series[cell.chart.seriesId];
+					if (series) {
+						var seriesData = series.data;
 						
 						var chartData = cell.chart.data;
-						chartData.push.apply(chartData, updatesData);
 						
-						while ((chartData[chartData.length-1].x - chartData[0].x) > cell.chart.seconds) {
-							chartData.shift();
+						var selectedData = [],
+							ic = seriesData.length,
+							i = ic-1;
+						
+						while (i >= 0 && (seriesData[ic-1].x - seriesData[i].x) <= cell.chart.ttl) {
+							selectedData.unshift(seriesData[i]);
+							--i;
 						}
+						
+						// Note: Update chart data in-place because our graph has a reference to it:
+						chartData.splice.apply(chartData, [ 0, chartData.length ].concat(selectedData));
+						
+						_this._stubChartData(chartData, cell.chart.ttl);
 						
 						cell.chart.graph.render();
 					}
@@ -114,8 +213,12 @@ _.extend(Dashboard.prototype, {
 		if (afterFn) { afterFn.call(_this, ctx, layout); }
 	},
 	
-	_renderLayout: function (layout, $wrapper) {
-		var _this = this;
+	_renderLayout: function () {
+		var _this = this,
+			layout = _this._layout,
+			$wrapper = _this.$el;
+		
+		$wrapper.empty();
 		
 		layout.$root = $wrapper;
 		
@@ -162,20 +265,9 @@ _.extend(Dashboard.prototype, {
 
 						$chart.appendTo($chartWrapper);
 						
-						if (!cell.chart.data) {
-							var stubData = [],
-								now = Math.floor((new Date()).getTime() / 1000);
-							
-							for (var ic = cell.chart.seconds, i = ic; i >= 1; --i) {
-								stubData.push({
-									x: now - i,
-									y: 0,
-									stub: true
-								});
-							}
-							
-							cell.chart.data = stubData;
-						}
+						var chartData = cell.chart.data = cell.chart.data || [];
+						
+						_this._stubChartData(chartData, cell.chart.ttl);
 						
 						var graph = cell.chart.graph = new Rickshaw.Graph({
 							element: $chart[0],
@@ -186,7 +278,7 @@ _.extend(Dashboard.prototype, {
 							series: [
 								{
 									color: cell.chart.color || 'rgba(0,0,0,1)',
-									data: cell.chart.data,
+									data: chartData,
 									name: (cell.header && cell.header.text) || cell.chart.name || cell.chat.seriesId
 								}
 							],
@@ -203,7 +295,7 @@ _.extend(Dashboard.prototype, {
 							graph: graph,
 							ticksTreatment: ticksTreatment,
 							timeUnit: {
-								seconds: Math.ceil(cell.chart.seconds / 5),
+								seconds: Math.ceil(cell.chart.ttl / 5),
 								formatter: function (d) {
 									return moment(d).format('HH:mm:ss');
 								}
@@ -222,12 +314,15 @@ _.extend(Dashboard.prototype, {
 			},
 			null
 		);
+		
+		_this._resizeLayout();
 	},
 	
-	_resizeLayout: function (layout) {
-		var _this = this;
+	_resizeLayout: function () {
+		var _this = this,
+			layout = _this._layout;
 		
-		_this._traverseLayout(_this.layout, {},
+		_this._traverseLayout(layout, {},
 			null,
 			function (ctx, layout, item, cell) {
 				if (cell && cell.chart && cell.chart.graph) {
@@ -248,6 +343,26 @@ _.extend(Dashboard.prototype, {
 			},
 			null
 		);
+	},
+	
+	_stubChartData: function (chartData, ttl) {
+		var nowX = Math.floor((new Date()).getTime() / 1000);
+		
+		if (chartData.length <= 0) {
+			chartData.push({
+				x: nowX,
+				y: 0,
+				stub: true
+			});
+		}
+		
+		while ((chartData[chartData.length-1].x - chartData[0].x) < ttl) {
+			chartData.unshift({
+				x: chartData[0].x - 1, //< Minus one second.
+				y: 0,
+				stub: true
+			});
+		}
 	}
 });
 
